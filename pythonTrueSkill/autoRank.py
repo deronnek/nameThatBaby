faf49b63b52b6ae2f456887215f02a17565d95e7
Andrew Owen <aowen@umn.edu>
2017-08-19 15:34:27 -0500

    test p value for completion

--- pythonTrueSkill/autoRank.py
+++ pythonTrueSkill/autoRank.py
@@ -183,7 +183,7 @@ def print_results():
 
 
 def is_sorted():
-    #results = get_results()
+    results = get_results()
 
     true = sorted(results, key=itemgetter('id'))
     current = sorted(results, key=itemgetter('mu'), reverse=True)
